31d4bdeaf97adf4aa115bb233594e9bf3475bd7e
Nikolai Kim <nikolai.kim@netcracker.com>
2023-05-10 22:16:27 +0200

    project refactoring

---
+++ models/niceFellowModel.js
@@ -0,0 +1,24 @@
+const sequelize = require('../db')
+const {DataTypes} = require('sequelize')
+
+
+const User = sequelize.define('User', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, unique: true, autoIncrement: true},
+    tgUserName: {type: DataTypes.STRING},
+    tgUserId: {type: DataTypes.STRING, unique: true},
+    niceFellowCount: {type: DataTypes.INTEGER, defaultValue: 0}
+})
+
+const Group = sequelize.define('Group', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, unique: true, autoIncrement: true},
+    tgGroupName: {type: DataTypes.STRING},
+    tgGroupId: {type: DataTypes.STRING, unique: true}
+}) 
+
+const UserGroup = sequelize.define('UserGroup', {})
+
+User.belongsToMany(Group, { through: UserGroup });
+Group.belongsToMany(User, { through: UserGroup });
+
+module.exports.User = User
+module.exports.Group = Group
