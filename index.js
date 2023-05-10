5ea9aa63fdfb23c4764b574104762362b971894e
Nikolai Kim <nikolai.kim@netcracker.com>
2023-05-10 10:24:41 +0200

    added steps for start action

--- index.js
+++ index.js
@@ -1,39 +1,89 @@
 const TelegramApi = require('node-telegram-bot-api')
 const token = '5918467905:AAHXL68CbUcGKG-wg75j-r1NExgBCDBLKTI'
 const sequelize = require('./db')
-const UserModel = require('./models')
+const TgModel = require('./models')
 
 const bot = new TelegramApi(token, {polling: true})
 
 const start = async () => {
-    //db connection
+    //_____DB CONNECTION_____//
     try {
         await sequelize.authenticate()
         await sequelize.sync()
     } catch (error) {
         console.log('Connection refused', error)
     }
-
+    
+    //_____LISTNER_____//
     bot.on('message', async msg => {
         const text = msg.text
-        const userId = msg.from.id
-        newUserId = userId.toString()
+        const userId = (msg.from.id).toString()
+        const userName = (msg.from.first_name).toString()
+        const chatType = (msg.chat.type).toString()
+        const groupId = (msg.chat.id).toString()
+        const groupName = (msg.chat.title).toString()
 
-        try {
-            if (text === '/start'){
-                const user = await UserModel.findOne({where: {userId: newUserId}})
-                console.log(user)
-    
-                if (!user){
-                    await UserModel.create({userId: newUserId})
-                    bot.sendMessage(msg.chat.id, 'Welcome!')
+        if (text === '/start'){
+
+            //console.log(msg)
+
+            //_____CHECK USER EXIST_____//
+            const user = await TgModel.User.findOne({
+                where: {
+                    tgUserId: userId
+                }
+            })
+
+            //_____CREATE USER IF NOT EXIST_____//
+            if (!user){
+                await TgModel.User.create({
+                    tgUserId: userId,
+                    tgUserName: userName
+                })
+                bot.sendMessage(msg.chat.id, 'Welcome!')
+            } else {
+                bot.sendMessage(msg.chat.id, 'U r alredy in the game!')
+            }
+
+            //_____STEPS FOR GROUP CHAT ONLY_____//
+            if (chatType === 'supergroup'){
+
+                //_____CHECK GROUP EXIST_____//
+                const group = await TgModel.Group.findOne({
+                    tgGroupId: groupId
+                })
+
+                //_____CREATE GROUP IF NOT EXIST_____//
+                if (!group){
+                    await TgModel.Group.create({
+                        tgGroupId: groupId,
+                        tgGroupName: groupName
+                    })
+                    bot.sendMessage(msg.chat.id, 'Group registred!')
                 } else {
-                    bot.sendMessage(msg.chat.id, 'U r alredy in the game!')
+                    bot.sendMessage(msg.chat.id, 'Group already in game!')
+                }
+
+                //_____KOSTYL FOR ADDING USER TO GROUP (NEED TO CREATE BETTER SOLUTION)_____//
+                const user = await TgModel.User.findOne({
+                    where: {
+                        tgUserId: userId
+                    }
+                })
+                const checkGroup = await TgModel.Group.findOne({
+                    tgGroupId: groupId
+                })
+
+                //_____ADD USER TO GROUP_____//
+                if (user){
+                    console.log(JSON.stringify(user, null, 2))
+                    await user.addGroup(checkGroup)
+                    bot.sendMessage(msg.chat.id, 'User added to group')
                 }
             }
-        } catch (error) {
-            return bot.sendMessage(msg.chat.id, 'Ooops...')
         }
+
+
     })
 }
 
